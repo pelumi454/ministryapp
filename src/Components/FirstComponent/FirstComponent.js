f3be6930946f6524aceb70632f49f60b2bdc89db
pelumi454 <amoochristianah454@gmail.com>
2021-01-15 15:35:14 +0000

    adding show more button

--- src/Components/FirstComponent/FirstComponent.js
+++ src/Components/FirstComponent/FirstComponent.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import './FirstComponent.css'
 import logo from '../../img/img1.png'
 
-const FirstComponent = () => {
+const FirstComponent = (props) => {
     return (
-        <div>
-            <img src={logo} class=""></img>
+        <div className="first-info ml-5">
+        <div className='title row pt-5'>
+            <div className="img-title img">
+            <img src={logo} className="text-left"></img>
+            </div>
+           <div className="text-left ml-3"><h1>{props.item.name}</h1></div>
+        </div>
+        <div className="pt-4">
+          <div className="minister-name text-left"><strong>Minister:</strong> {props.item.minister}</div>  
+        </div>
+        <div className="minister-name text-left pt-3"><strong>Minister of State:</strong> {props.item.minister_state}</div>
+        <div className="hr-line"><hr/></div>
         </div>
     )
 }
--- src/Components/MainComponent/MainComponent.js
+++ src/Components/MainComponent/MainComponent.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react'
 import FirstComponent from '../FirstComponent/FirstComponent'
+import SecondComponent from '../SecondComponent/SecondComponent'
 import axios from 'axios'
 
 class MainComponent extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             items: []
+        }
+    }
+
+    componentDidMount (){
+        axios.get('https://ministries.herokuapp.com/ministries/?format=json')
+        .then(res=> {
+            const items = res.data
+            console.log(items)
+             this.setState({
+                items: items
+              })
+     
+        })
+        .catch(error => {
+            console.log(error)
+        })
+          
+        }
+    
     render() {
+        const {items} = this.state
         return (
             <div>
-                <FirstComponent/>
+                <div>
+                {
+                        items.map(item => (
+                            <FirstComponent item={item}  />
+                        ))
+                    } 
+                </div>
+                
+                <div>
+                {
+                        items.map(item => (
+                            <SecondComponent item={item}  />
+                        ))
+                    } 
+                </div>
+                
+
+                    
             </div>
         )
     }
